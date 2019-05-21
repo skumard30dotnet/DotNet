a399b7c2bdbff743cbdd858ccdd71c6033fc7e38
Sasi Kumar <skumard30@outlook.com>
2019-05-21 21:40:42 +0530

    Commit

---
+++ ClassandObjects/Calculator.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandObjects
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Calculator
+    {
+        int a = 0;
+        int b = 0;
+        int result = 0; // Data Member
+
+        public Calculator()
+        {
+
+        }
+
+        public Calculator(int x, int y)
+        {
+            a = x;
+            b = y;
+        }
+
+
+        /// <summary>
+        /// Addition of two numbers
+        /// </summary>
+        /// <param name="a">passing integer datatype</param>
+        /// <param name="b"></param>
+        public void Add()
+        {
+            result = a + b;
+            PrintValues(a, b, result, "Sum");
+        }
+
+        public void Sub()
+        {
+            result = a - b;
+            PrintValues(a, b, result, "Sub");
+            //Console.WriteLine("The sub of a:{0} , b:{1} = {2}", a, b, result);
+        }
+
+        public void Mul()
+        {
+            result = a * b;
+            PrintValues(a, b, result, "Mul");
+            //Console.WriteLine("The Mul of a:{0} , b:{1} = {2}", a, b, result);
+        }
+
+        public void Div()
+        {
+            result = a / b;
+            PrintValues(a, b, result, "Div");
+
+            //Console.WriteLine("The sub of a:{0} , b:{1} = {2}", a, b, result);
+        }
+
+        void PrintValues(int a, int b, int result, string type)
+        {
+            Console.WriteLine("The value of A is {0}", a);
+            Console.WriteLine("The value of B is {0}", b);
+            Console.WriteLine("The {0} is : {1}", type, result);
+            Console.WriteLine("**********************************************");
+        }
+
+        /*
+        /// <summary>
+        /// Addition of two numbers
+        /// </summary>
+        /// <param name="a">passing integer datatype</param>
+        /// <param name="b"></param>
+        public void Add(int a, int b) 
+        {
+            result = a + b;
+            PrintValues(a, b, result);
+        }
+
+        public void Sub(int a, int b)
+        {
+            result = a - b;
+            PrintValues(a, b, result);
+            //Console.WriteLine("The sub of a:{0} , b:{1} = {2}", a, b, result);
+        }
+
+        public void Mul(int a, int b)
+        {
+            result = a * b;
+            PrintValues(a, b, result);
+            //Console.WriteLine("The Mul of a:{0} , b:{1} = {2}", a, b, result);
+        }
+
+        public void Div(int a, int b)
+        {
+            result = a / b;
+            PrintValues(a, b, result);
+            
+            //Console.WriteLine("The sub of a:{0} , b:{1} = {2}", a, b, result);
+        }
+
+        void PrintValues(int a, int b, int result)
+        {
+            Console.WriteLine("The value of A is {0}", a);
+            Console.WriteLine("The value of B is {0}", b);
+            Console.WriteLine("The result is : {0}", result);
+            Console.WriteLine("**********************************************");
+        }
+        */
+    }
+}
---
+++ ClassandObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+
+            Console.WriteLine("Enter the value of A");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of B");
+            b = int.Parse(Console.ReadLine());
+
+            Calculator calc = new Calculator(a, b);
+           
+
+            calc.Add();
+            calc.Sub();
+            calc.Mul();
+            calc.Div();
+
+            //calc.Add(a, b);
+            //calc.Sub(a, b);
+            //calc.Mul(a, b);
+            //calc.Div(a, b);
+
+            Console.Read();
+        }
+    }
+}
