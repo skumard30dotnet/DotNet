4dbed0c283631901ddbd14cc8a23dcba15611cb1
Sasi Kumar <skumard30@outlook.com>
2019-05-30 08:15:05 +0530

    Partial Class

---
+++ PartialClass/PartialClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    partial class PartialClass
+    {
+        public void Create()
+        {
+            Console.WriteLine("Created Successfully");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Updated Successfully");
+        }
+    }
+
+    partial class PartialClass
+    {
+        public void Get()
+        {
+            Console.WriteLine("Loaded Successfully");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Deleted Successfully");
+        }
+    }
+
+}
